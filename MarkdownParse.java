75d2f4d6c45808ffdac4d9223db767d678ecdf54
k3alvare <97643301+k3alvare@users.noreply.github.com>
2022-01-28 11:44:52 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,7 +17,6 @@ public class MarkdownParse {
             int closeParen = markdown.indexOf(")", openParen);
 
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
     
         }
         return toReturn;
